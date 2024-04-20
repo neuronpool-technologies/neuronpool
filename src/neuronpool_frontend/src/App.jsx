e1b6158a98f525185ec4d0e9336735be08308b47
jesssekeogh <jesse.keogh74@gmail.com>
2024-04-20 19:38:20 +0100

    project setup

--- src/neuronpool_frontend/src/App.jsx
+++ src/neuronpool_frontend/src/App.jsx
@@ -1,31 +1,9 @@
-import { useState } from 'react';
-import { neuronpool_backend } from 'declarations/neuronpool_backend';
+import React from 'react'
 
-function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    neuronpool_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
+const App = () => {
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
+    <div>Hello world</div>
+  )
 }
 
-export default App;
+export default App
--- src/neuronpool_frontend/src/main.jsx
+++ src/neuronpool_frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: props.colorMode === "dark" ? "#1b2030" : "#f1f1f3",
+      },
+    }),
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 );

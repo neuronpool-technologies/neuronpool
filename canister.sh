e461eca37afdc555cb2acf3576bfd6265162efcf
jesssekeogh <jesse.keogh74@gmail.com>
2024-05-28 21:38:39 +0100

    adding useful dfx commands

--- canister.sh
+++ canister.sh
@@ -7,7 +7,12 @@
 # dfx canister --network ic status neuronpool
 
 # 
-# canister test functions:
+# useful canister commands:
 # 
 
-# dfx canister --network ic call neuronpool get_canister_accounts
+# dfx canister --network ic call neuronpool get_protocol_information
+# dfx canister --network ic call neuronpool get_operation_history
+# dfx canister --network ic call neuronpool get_neuron_information
+# dfx canister --network ic call neuronpool get_main_neuron
+
+# dfx canister --network ic call neuronpool controller_get_canister_memory
